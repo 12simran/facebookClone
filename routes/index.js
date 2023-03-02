c4848c66d401db8ffcbf051f87e2bdd7ee4373bc
12simran <simrankaur283380@gmail.com>
2023-03-02 17:23:37 +0530

    first controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+ const homeController = require('../controllers/homeController')
 
 const router = express.Router()
 
 console.log("router is working");
 
+router.get('/',homeController.home)
+
 module.exports = router
+
