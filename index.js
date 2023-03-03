ec219d64c254fe5e1a7e605dca21ef2672b3d677
12simran <simrankaur283380@gmail.com>
2023-03-03 17:43:30 +0530

    second controller

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const expressLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs')
 app.set('views','./views')
 
 const router = require('./routes/')
+app.use(expressLayouts)
 app.use('/', router )
 
 app.listen(port, () => {
