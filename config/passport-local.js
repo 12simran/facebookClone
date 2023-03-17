b538da0f46aefabf27a25501395090dcc87d6929
12simran <simrankaur283380@gmail.com>
2023-03-17 16:38:51 +0530

    sign in

---
+++ config/passport-local.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var passport = require('passport');
+const User = require('../models/user');
+var LocalStrategy = require('passport-local').Strategy
+
+
+passport.use(new LocalStrategy({usernameField:'email'},
+    function (email, password, done) {
+        User.findOne({ email : email },function (err, user){
+      if (err) { return done(err); }
+      if(!user || user.passsword!=password) { return done(null,false);}
+
+      return done(null, user);
+        });
+    }
+));
+//serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user,done){
+      done(null,user.id);
+})
+
+
+//deserializing the user from the key in the cookies
+passport.deserializeUser(function(id,done){
+  User.findById(id,function(err,user){
+    if(err){
+      console.log(err);
+    }
+    return done(null,user)
+  })
+})
+passport.checkAuthentication=function(req,res,next){
+  if(req.isAuthenticated()){
+   return   next()
+  }
+  return res.redirect('/users/sign-in')
+}
+ 
+passport.setAuthenticatedUser=function(req,res,next){
+  if(req.isAuthenticated()){
+    res.locals.user = req.user
+  }
+  next();
+}
+
+
+module.exports = passport;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const userController = require('../controllers/userController')
 const router = express.Router()
+const passport = require('passport')
 
 
 router.get('/profile',userController.profile)
 router.get('/sign-up',userController.signUp)
 router.get('/sign-in',userController.signIn)
+
 router.post('/create',userController.create)
-router.post('/signin',userController.login)
+router.post('/signin',passport.authenticate('local',{failureRedirect:'/users/sign-in'}) ,
+ userController.login)
 
 
 
